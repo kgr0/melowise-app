220f02566990f0206b27ce3871b3093767a9b424
Kateryna Grytsko <katia.grytsko@student.uj.edu.pl>
2023-05-16 22:09:12 +0200

    exception and wrong options names fixed

--- screens/interval/interval.py
+++ screens/interval/interval.py
@@ -7,8 +7,6 @@ from db.dtos import QuestionDTO
 import random
 from kivy.properties import StringProperty
 from kivy.core.audio import SoundLoader
-from pydub import AudioSegment
-import numpy as np
 from core.sound_generator import generate
 import constants
 
@@ -37,9 +35,8 @@ class IntervalScreen(Screen):
         self.load_question()
 
     def load_question(self):
-        #self.sound = SoundLoader.load('././sounds/piano_c.mp3') # Load the sound file
-        random_key, random_value = random.choice(list(constants.INTERVALS.items()))
-        self.interval = random.choice(random_key)
+        (random_key, random_value) = random.choice(list(constants.INTERVALS.items()))
+        self.interval = random_key
         root_note = random.choice(constants.PITCHES) + "4"
         
         self.generate_sound(root_note, random_value)
@@ -54,9 +51,11 @@ class IntervalScreen(Screen):
     
 
     def generate_sound(self,root_note, interval ):
-        generate(root_note, interval)
-        sound = AudioSegment.from_wav('././data/sound_1.wav')
-        #sound.export('././data/melody.mp3', format='mp3') # Export to mp3 to avoid platform-specific issues
+        try:
+            generate(root_note, interval)
+        except KeyError:
+            #TODO fix flat problem
+            self.load_question()
         self.sound = SoundLoader.load('././data/sound_1.wav')
 
     def play_sound(self):
@@ -66,10 +65,9 @@ class IntervalScreen(Screen):
     def generate_answers(self,correct_answer):
         answers = [correct_answer]
         while len(answers) < 4:
-            random_key, random_value = random.choice(list(constants.INTERVALS.items()))
-            answer = random.choice(random_key)
-            if answer not in answers:
-                answers.append(answer)
+            (random_key, random_value) = random.choice(list(constants.INTERVALS.items()))
+            if random_key not in answers:
+                answers.append(random_key)
         random.shuffle(answers)
         return answers
 
