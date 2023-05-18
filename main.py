b8c945ba1abf737d473177a2ddfa91ca581b5962
Kateryna Grytsko <katia.grytsko@student.uj.edu.pl>
2023-05-18 23:17:56 +0200

    add bars chart

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-# import kivy
-# from kivy.app import App
-# from kivy.lang import Builder
-
-# from screens.home.home import HomeScreen
-# from kivy.uix.screenmanager import ScreenManager, Screen
-
-# kivy.require('1.9.0')
-
-# class MeloWise(App):
-    
-#     def build(self):
-#         Builder.load_file('views/home.kv')
-#         return HomeScreen()
-
-# melowise = MeloWise()
-# melowise.run()
-
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
@@ -34,13 +16,10 @@ Builder.load_file('screens/statistics/statistics.kv')
 
 class MyApp(MDApp):
     def build(self):
-        # Builder.load_file('screens/home/home.kv')
-        # return HomeScreen()
         sm = ScreenManager()
         sm.add_widget(HomeScreen(name='home'))
         sm.add_widget(IntervalScreen(name='interval'))
         sm.add_widget(StatisticsScreen(name='statistics'))
-        #sm.current = 'home'
         return sm
 
 if __name__ == '__main__':
--- screens/statistics/statistics.py
+++ screens/statistics/statistics.py
@@ -1,8 +1,11 @@
+from collections import Counter
 from kivy.uix.screenmanager import Screen
 from db.db import DatabaseHandler
 import matplotlib.pyplot as plt
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivy
 from kivy.uix.floatlayout import FloatLayout
+import numpy as np
+import constants
 
 class StatisticsScreen(Screen, FloatLayout):
     def __init__(self, **kwargs):
@@ -24,6 +27,8 @@ class StatisticsScreen(Screen, FloatLayout):
 
     def update_chart(self):
         answers = self.get_answers_from_db()
+        if(len(answers) == 0):
+            return
         labels = ['Correct', 'Incorrect']
         values = [
             len([obj for obj in answers if obj.user_answer == obj.correct_answer]),
@@ -35,6 +40,44 @@ class StatisticsScreen(Screen, FloatLayout):
         ax.pie(values, labels=labels, colors=colors, autopct='%1.1f%%', textprops={'color':"w"})
         ax.set_title('Pie Chart')
         chart_widget = FigureCanvasKivy(fig)
-        chart_container = self.ids.chart_container
+        chart_container = self.ids.pie_chart_container
         chart_container.clear_widgets()
         chart_container.add_widget(chart_widget)
+
+
+        #print(list(set(question.correct_answer for question in answers)))
+        #intervals = list(set(question.correct_answer for question in answers))
+
+        intervals = list(constants.INTERVALS.keys())
+        correct_answers, wrong_answers = np.zeros(len(intervals)),  np.zeros(len(intervals))
+
+        correct_answers = [len([ answer for answer in answers if answer.correct_answer == interval and answer.correct_answer == answer.user_answer]) for interval in intervals]
+        wrong_answers = [len([ answer for answer in answers if answer.correct_answer == interval and answer.correct_answer != answer.user_answer]) for interval in intervals]
+
+        penguin_means = {
+            'Correct': correct_answers,
+            'Wrong': wrong_answers,
+        }
+
+        x = np.arange(len(intervals))  # the label locations
+        width = 0.25  # the width of the bars
+        multiplier = 0
+
+        fig, ax = plt.subplots(layout='constrained')
+
+        for attribute, measurement in penguin_means.items():
+            offset = width * multiplier
+            rects = ax.bar(x + offset, measurement, width, label=attribute)
+            ax.bar_label(rects, padding=3)
+            multiplier += 1
+
+        # Add some text for labels, title and custom x-axis tick labels, etc.
+        #ax.set_ylabel('Length (mm)')
+        #ax.set_title('Penguin attributes by intervals')
+        ax.set_xticks(x + width, intervals)
+        ax.set_ylim(0, max(max(correct_answers), max(wrong_answers))+2)
+
+        bars_chart_widget = FigureCanvasKivy(fig)
+        bars_chart_container = self.ids.bars_chart_container
+        bars_chart_container.clear_widgets()
+        bars_chart_container.add_widget(bars_chart_widget)
