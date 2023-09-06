2d079b99227e4510949a8311216eb0deb52f0346
Kateryna Grytsko <katia.grytsko@student.uj.edu.pl>
2023-09-06 18:57:51 +0200

    change statustics view

--- screens/statistics/statistics.py
+++ screens/statistics/statistics.py
@@ -4,6 +4,7 @@ from db.db import DatabaseHandler
 import matplotlib.pyplot as plt
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivy
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
 import numpy as np
 import constants
 
@@ -35,17 +36,35 @@ class StatisticsScreen(Screen, FloatLayout):
             len([obj for obj in answers if obj.user_answer != obj.correct_answer])
         ]       
         colors = ['#7abd7e', '#ff6961']
+        explode = (0.05,0.05)
         fig, ax = plt.subplots()
-        ax.pie(values, labels=labels, colors=colors, autopct='%1.1f%%', textprops={'color':"w"})
-        ax.set_title('Pie Chart')
+
+        ax.pie(values, labels=labels, colors=colors, autopct='%1.1f%%', pctdistance=0.85, startangle=90, explode = explode)
+
+        centre_circle = plt.Circle((0,0),0.70,fc='white')
+        fig = plt.gcf()
+        fig.gca().add_artist(centre_circle)
+
+        #ax.pie(values, labels=labels, colors=colors, autopct='%1.1f%%')
+        
+        #ax.set_title('Pie Chart')
         chart_widget = FigureCanvasKivy(fig)
         chart_container = self.ids.pie_chart_container
         chart_container.clear_widgets()
+
+        # pie_chart_label = Label(
+        #     text= labels[0] + ' ' + str(round((values[0]/len(answers))*100))+'% ' + labels[1] + ' ' + str(100 - round((values[0]/len(answers))*100)) + '%',
+        #     font_size=20,
+        #     color=(.788, .521, .517, 1),
+        #     halign='center',
+        #     valign='middle',
+        #     size_hint=(None, None), 
+        #     size=(300, 44)
+        # )
+        # chart_container.add_widget(pie_chart_label)
         chart_container.add_widget(chart_widget)
-
-
-        #print(list(set(question.correct_answer for question in answers)))
-        #intervals = list(set(question.correct_answer for question in answers))
+        
+        
 
         intervals = list(constants.INTERVALS.keys())
         correct_answers, wrong_answers = np.zeros(len(intervals)),  np.zeros(len(intervals))
@@ -62,7 +81,7 @@ class StatisticsScreen(Screen, FloatLayout):
         width = 0.25  # the width of the bars
         multiplier = 0
 
-        fig, ax = plt.subplots(layout='constrained')
+        fig2, ax2 = plt.subplots(layout='constrained')
 
 
         colors = {
@@ -71,17 +90,15 @@ class StatisticsScreen(Screen, FloatLayout):
         }
         for attribute, measurement in penguin_means.items():
             offset = width * multiplier
-            rects = ax.bar(x + offset, measurement, width, label=attribute, color=colors[attribute])
-            ax.bar_label(rects, padding=3)
+            rects = ax2.bar(x + offset, measurement, width, label=attribute, color=colors[attribute])
+            ax2.bar_label(rects, padding=3)
             multiplier += 1
+        
+        formatted_intervals = [label.replace(" ", "\n") for label in intervals]
+        ax2.set_xticks(x + width, formatted_intervals)
+        ax2.set_ylim(0, max(max(correct_answers), max(wrong_answers))+2)
 
-        # Add some text for labels, title and custom x-axis tick labels, etc.
-        #ax.set_ylabel('Length (mm)')
-        #ax.set_title('Penguin attributes by intervals')
-        ax.set_xticks(x + width, intervals)
-        ax.set_ylim(0, max(max(correct_answers), max(wrong_answers))+2)
-
-        bars_chart_widget = FigureCanvasKivy(fig)
+        bars_chart_widget = FigureCanvasKivy(fig2)
         bars_chart_container = self.ids.bars_chart_container
         bars_chart_container.clear_widgets()
         bars_chart_container.add_widget(bars_chart_widget)
